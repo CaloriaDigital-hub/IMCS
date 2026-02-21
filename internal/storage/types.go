ef7630fee5e7be7f3526e173e1e6879cec4b84cd
asanjs <asanbek.toktobaev.dev@gmail.com>
2026-02-21 20:06:53 +0600

    feat: стандтартный sync.RWMutex был заменен на кастомный атомарный блокатор

--- internal/storage/types.go
+++ internal/storage/types.go
@@ -1,19 +1,61 @@
 package storage
 
 import (
-	"sync"
+	"runtime"
+
 	"sync/atomic"
 	"time"
 
 	"imcs/internal/storage/cold"
 )
 
-// Persistence — интерфейс для персистенции данных.
+/*
+	Кастомная легковесная блокировка
+	0: свободен, >0: кол-во readers, -1: писатель захватил
+*/
+type SpinRWMutex int32
+
+func (m *SpinRWMutex) RLock () {
+	for i := 0; ; i++ {
+		if i > 30 {
+			runtime.Gosched()
+		}
+
+		v:=atomic.LoadInt32((*int32)(m))
+
+		if v >= 0 && atomic.CompareAndSwapInt32((*int32)(m), v, v+1) {
+			break
+		}
+	}
+}
+func (m *SpinRWMutex) RUnlock() {
+	atomic.AddInt32((*int32)(m), -1)
+}
+
+
+
+func (m *SpinRWMutex) Lock() {
+	for i := 0; !atomic.CompareAndSwapInt32((*int32)(m), 0, -1); i++ {
+		if i > 30 {
+			runtime.Gosched()
+		}
+	}
+}
+
+func (m *SpinRWMutex) Unlock() {
+	atomic.StoreInt32((*int32)(m), 0)
+}
+
+
+// Persistence — интерфейс для персистенции данных
 type Persistence interface {
 	Write(cmd, key, value string, duration time.Duration) error
 }
 
-// Item — элемент кеша.
+
+
+
+// Item — элемент кеша
 type Item struct {
 	Key        string
 	Value      string
@@ -22,15 +64,22 @@ type Item struct {
 	HeapIndex  int
 }
 
-// priorityQueue — очередь с приоритетом для TTL.
+// priorityQueue — очередь с приоритетом для TTL
 type priorityQueue []*Item
+
+
+
+
 
 // shard — один шард кеша.
 type shard struct {
-	sync.RWMutex
+	SpinRWMutex
 	items map[string]*Item
 	pq    priorityQueue
 }
+
+
+
 
 // coldItem — элемент для передачи в cold storage через канал.
 type coldItem struct {
@@ -39,12 +88,16 @@ type coldItem struct {
 	expireAt int64
 }
 
+
+
+
+
 // Cache — шардированное in-memory хранилище.
 type Cache struct {
 	shards    [shardCount]*shard
 	persister Persistence
-	cold      *cold.Store   // холодное хранилище на диске
-	flushCh   chan coldItem // канал для worker pool (выгрузка на диск)
+	cold      *cold.Store  
+	flushCh   chan coldItem
 	maxKeys   int64
 	totalKeys atomic.Int64
 	stopCh    chan struct{}
